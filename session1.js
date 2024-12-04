694f2540e49c163f9e74c6ecf797eb2593f7862e
Deepak Gupta <deepakgupta60@gmail.com>
2024-12-04 21:09:09 +0530

    working

--- session1.js
+++ session1.js
@@ -38,3 +38,22 @@ function findQuad()
   return n*(n+1)/2
  
 }
+
+
+// find max / or min
+
+function findQuad()
+{
+  let arr = [2,3,20,5,10]
+  let n1=arr[0]
+  
+  for(let i=0;i<arr.length;i++)
+    {
+      if(n1>arr[i]) // 2< 
+        {
+          n1=arr[i]
+        }
+    }
+  return n1
+ 
+}
